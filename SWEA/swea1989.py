a7d4be01cc5333431ad71402ae375afb6991f4c0
water9309 <72597437+water9309@users.noreply.github.com>
2022-07-05 00:40:53 +0900

    2022-07-05 00:40

---
+++ SWEA/swea1989.py
@@ -0,0 +1,11 @@
+N = int(input())
+word = []
+
+for i in range(N):
+    word.append(input())
+
+for i in range(N):
+    if list(word[i]) == list(''.join(reversed(word[i]))):
+        print("#{}".format(i+1), "1")
+    else:
+        print("#{}".format(i+1), "0")

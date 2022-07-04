a08a37a1fc714322ae4a6d2299010d421967b203
water9309 <72597437+water9309@users.noreply.github.com>
2022-07-05 00:03:40 +0900

    2022-07-05 00:03

--- SWEA/swea1946.py
+++ SWEA/swea1946.py
@@ -4,6 +4,18 @@ sys.stdin = open("input.txt", "r")
 
 T = int(sys.stdin.readline())
 
-N = int(sys.stdin.readline())
+for i in range(T):
+    N = int(sys.stdin.readline())
+    print("#{}".format(i+1))
+    ans = ""
+    for _ in range(N):
+        Ci, Ki = sys.stdin.readline().split()
+        Ki = int(Ki)
+        ans += Ci*Ki
 
-print(T, N)
+    for i in range(len(ans)):
+        if (i+1) % 10 != 0:
+            print(ans[i], end="")
+        else:
+            print(ans[i])
+    print()
